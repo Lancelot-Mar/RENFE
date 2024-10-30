0c1e9b3d9216c875155afb1a3f24f51f5927d63e
Lancelot-Mar <leomarescutti247@gmail.com>
2024-10-30 10:59:50 +0100

    10:59

--- Ejercicio2.c
+++ Ejercicio2.c
@@ -12,28 +12,9 @@
 
 int main(){
 
-	float temperatura[8] = {lunes,martes,miercoles,jueves,viernes,sabado,domingo};
+char dias[8][10] = {"lunes", "martes", "miercoles", "jueves", "viernes", "sabado", "domingo"}
 	
-	printf("Dime la temperatura del lunes: \n");
-	scanf("%f\n", &lunes);
 	
-	printf("Dime la temperatura del martes: \n");
-	scanf("%f\n", &martes);
-
-	printf("Dime la temperatura del miercoles: \n");
-	scanf("%f\n", &miercoles);
-
-	printf("Dime la temperatura del jueves: \n");
-	scanf("%f\n", &jueves);
-
-	printf("Dime la temperatura del viernes: \n");
-	scanf("%f\n", &viernes);
-
-	printf("Dime la temperatura del sabado: \n");
-	scanf("%f\n", &sabado);
-
-	printf("Dime la temperatura del domingo: \n");
-	scanf("%f\n", &domingo);
 
 	return 0;
 }
