f90802dafeb5de6a4d7bb1660a9a0c8ba4e0ef67
bajr06 <bryanandreujimenezrojas@gmail.com>
2024-10-30 10:59:47 +0100

    11:00

--- Ejercicio2.c
+++ Ejercicio2.c
@@ -12,28 +12,17 @@
 
 int main(){
 
-	float temperatura[8] = {lunes,martes,miercoles,jueves,viernes,sabado,domingo};
-	
-	printf("Dime la temperatura del lunes: \n");
-	scanf("%f\n", &lunes);
-	
-	printf("Dime la temperatura del martes: \n");
-	scanf("%f\n", &martes);
 
-	printf("Dime la temperatura del miercoles: \n");
-	scanf("%f\n", &miercoles);
 
-	printf("Dime la temperatura del jueves: \n");
-	scanf("%f\n", &jueves);
-
-	printf("Dime la temperatura del viernes: \n");
-	scanf("%f\n", &viernes);
-
-	printf("Dime la temperatura del sabado: \n");
-	scanf("%f\n", &sabado);
-
-	printf("Dime la temperatura del domingo: \n");
-	scanf("%f\n", &domingo);
+	float temperatura[8][10] = {
+		"Lunes",
+		"Martes",
+		"Miércoles",
+		"Jueves",
+		"Viernes",
+		"Sábado",
+		"Domingo"
+	};
 
 	return 0;
 }
