72759db99c118465060672eb96a722385c913de9
Lancelot-Mar <leomarescutti247@gmail.com>
2024-10-30 14:02:37 +0100

    Fin Clase

--- Ejercicio2.c
+++ Ejercicio2.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <math.h>
 #include <time.h>
+#include <string.h>
 
 #define DIAS 7
 
@@ -15,18 +16,30 @@
 int main(){
 	char dia[7][10] = {"lunes", "martes", "miercoles", "jueves", "viernes", "sábado", "domingo"};
 	float temperatura[7] = {1, 2, 3, 4, 5 ,6 ,7};
-	float promedio = 0;
+	float promedio = 0, mayor = 0, ;
+	char dma[10];
+	char dme[10];
 
 	for(int i = 0; i < DIAS; i++){
-			printf("Escribe la temperatura del %s\n",dia[i]);
-			scanf("%f", &temperatura[i]);
-			for(int j = temperatura[i]; j < 100){
-			promedio =+ j;
-			}
+		printf("Escribe la temperatura del %s\n",dia[i]);
+		scanf("%f", &temperatura[i]);
+		promedio += temperatura[i];
+		menor += temperatura[i];
+
+		if(temperatura[i] > mayor){
+			mayor = temperatura[i];
+		} 
+		else
+		if(temperatura[i] < menor){
+			menor = temperatura[i];
+		}
 	}
 
-	promedio = promedio;
-	printf("Temperatura promedio de la semana:%f\n", promedio);
+	promedio = promedio/DIAS;
 
-	return 0;
+	printf("Temperatura promedio de la semana:%f\n", promedio);
+	printf("Temperatura máxima: %f°C el día %s\n·", mayor, dma);
+	printf("Temperatura mínima: %f°C el día %s\n.", menor, dme);
+
+	return EXIT_SUCCESS;
 }
