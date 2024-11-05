f4dc14eb03700e4cf859e1e5f6402af7c0462691
bajr06 <bryanandreujimenezrojas@gmail.com>
2024-11-05 14:15:34 +0100

    Avance Práctica 3

--- Práctica3.c
+++ Práctica3.c
@@ -3,6 +3,8 @@
 #include <math.h>
 #include <time.h>
 
+#define LIM 10	
+
 /*
  * 
  * 	Autor: Bryan Andreu Jiménez Rojas y Lenardo Marescutti Mariñas.
@@ -11,5 +13,25 @@
 */ 
 
 int main(){
+	int gestion_producto;
+	int cantidad[LIM];
+	char nombre_producto[LIM][20];
+	
+	printf("¿Cuantos productos quieres gestionar?\n");
+	scanf(" %d", &gestion_producto);
+	
+	if(gestion_producto <= LIM){
+		for (int i = 0; i < gestion_producto; i++){
+			printf("Escriba el nombre del producto:");
+			scanf("%s", nombre_producto[i]);
+
+			printf("Escriba la cantidad de stock del producto:");
+			scanf("%d", &cantidad[i]);
+		}
+	}
+	else{
+		printf("Solo puedes gestionar un total del 10\n");
+	}
+
 	return EXIT_SUCCESS;
 }
