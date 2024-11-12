9f73cc3f2a3cbf6fa44d3307542bfedaabf89c8e
Leonardo Marescutti <155202752+Lancelot-Mar@users.noreply.github.com>
2024-11-12 15:59:03 +0100

    Update Ejercicio2.c

--- Ejercicio2.c
+++ Ejercicio2.c
@@ -12,22 +12,25 @@
 */ 
 
 int main() {
-    int n_palabras = 8;
-    char palabras[n_palabras][20] = {"Hola","que","tal"};  // Array de palabras de tamaño dinámico
+    int n_palabras = 5; //Cantidad de palabras en el array
+    char palabras[n_palabras][20] = {"Hola","que","tal","como","estas"};  // Array de palabras 
 
-    printf("¿Cuántas palabras tiene la lista?\n");
-    scanf("%d", &n_palabras);
+    //printf("¿Cuántas palabras tiene la lista?\n");
+    //scanf("%d", &n_palabras); Para hacer que el tamaño del array sea dinamico
+	
+    //for (int i = 0; i < n_palabras; i++) {
+    //    printf("Dime una palabra:\n");
+    //    scanf("%s", palabras[i]);
+    //}
 
-    for (int i = 0; i < n_palabras; i++) {
-        printf("Dime una palabra:\n");
-        scanf("%s", palabras[i]);
-    }
-
+    //Creamos un bucle que vaya palabra por palabra guardando las palabras que no contengan el valor a,A.
     printf("Palabras sin la letra 'a':\n");
     for (int j = 0; j < n_palabras; j++) {
+	// saltamos las palabras que contengan lo siguiente:
         if (strcmp(palabras[j], "a") == 0 || strcmp(palabras[j], "A") == 0) { 
 		continue;
         }
+	// si no tiewnen ese valor procerderemos a imprimirlas poor pantalla
 	else{
 		printf("%s\n", palabras[j]);
     	}
