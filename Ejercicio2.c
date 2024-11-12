68b9a3e505854865755637e95c0a776b8312c921
bajr06 <181849388+bajr06@users.noreply.github.com.>
2024-11-12 19:41:31 +0100

    Finalziación de la Práctica 5

--- Ejercicio2.c
+++ Ejercicio2.c
@@ -13,17 +13,23 @@
 */ 
 
 int main() {
-    char palabras[LIM][20] = {"Hola","que","tal","como","estas"};  // Array de palabras 
+    	char palabras[LIM][20] = {"Hola", "que", "tal", "como", "estas"};  // Array de palabras 
 
-    //Creamos un bucle que vaya palabra por palabra guardando las palabras que no contengan el valor a,A.
-    printf("Palabras sin la letra 'a':\n");
-    for (int j = 0; j < LIM; j++) {
-	// saltamos las palabras que contengan lo siguiente:
-        if (strcmp(palabras[j], "a") == 0 || strcmp(palabras[j], "A") == 0) { 
-		continue;
-        }
-	// si no tiewnen ese valor procerderemos a imprimirlas poor pantalla
-		printf("%s\n", palabras[j]);
-    }
+    	printf("Palabras sin la letra 'a':\n");
+    		for (int j = 0; j < LIM; j++) { // Este bucle pasa por cada candena de carácteres del Array
+        	// La condional cumple que si encontramos 'a' o 'A'
+        	if (strchr(palabras[j], 'a') || strchr(palabras[j], 'A')){
+            		continue; // Se saltará esa palabra y reiniciará el bucle
+        	}
+		else{ //Y si no es así
+        		printf("%s\n", palabras[j]); // La imprimirá
+    		}
+	}
     return 0;
 }
+
+/* Cabe recalcar que en este programa estamos usando 
+ * la librería "string.h" para poder 
+ * usar la funcion "strchr()", encargada de buscar 
+ * un carácter dentro de una cadena de carácteres
+*/
