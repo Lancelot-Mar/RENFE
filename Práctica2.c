73b68f3706f376daf9381cf5c1614f8415f96e2f
Leonardo Marescutti <155202752+Lancelot-Mar@users.noreply.github.com>
2024-11-12 16:03:37 +0100

    Update Práctica2.c

--- Práctica2.c
+++ Práctica2.c
@@ -17,8 +17,9 @@ int main(){
 	int cantidad_suspensos = 0, cantidad_aprobados = 0,npromedio = 0, nsuspenso = 0, naprobados = 0;
 
 	printf("Ingrese la cantidad de estudiantes: \n");
-	scanf(" %d", &cantidad_estudiantes);
-
+	scanf(" %d", &cantidad_estudiantes); //guardamos la cantidad de estudiantes
+	
+	//Creamos un bucle que vaya pasando nota por nota preguntando y guardando las notas de todos los alumnos.
 	for(int i = 1; i <= cantidad_estudiantes; i++){
 		printf("Escribe la nota del estudiante:\n");
 		scanf(" %f", &nota[i]); //guardamos el valor de las notas
@@ -34,24 +35,26 @@ int main(){
 		}
 
 		if(nota[i] > aprobados){
-			aprobados = nota[i];
-			naprobados = i; 
+			aprobados = nota[i]; //guardamos el numero mas grande
+			naprobados = i; //guardamos el numero de posicion del numero mas grande
 		}
 		else
 		if(nota[i] < suspenso){
-			suspenso = nota[i];
+			suspenso = nota[i]; //guardamos el numero mas pequeño
 			nsuspenso = i; //guardamos el numero de posicion del numero mas pequeño
 		}
 	}
 
 	promedio = promedio/cantidad_estudiantes; //dividimos la variable promedio por la cantidad de estudiantes
-	
+
+	//Imprimimos por pantalla:
 	printf("Promedio de notas:%.2f\n", promedio);
 	printf("Número de estudiantes aprobados:%d\n", cantidad_aprobados);
 	printf("Número de estudiantes suspendidos:%d\n", cantidad_suspensos);
 	printf("Nota máxima: %.2f (Estudiante en la posición %d)\n", aprobados, naprobados);
 	printf("Mota minima: %.2f (Estudiante en la posición %d)\n", suspenso, nsuspenso);
 
+	//Por último, creamos un nuevo bucle que imprima por pantalla las notas que mayores al promedio
 	for(int j = 1; j <= cantidad_estudiantes; j++){
 		if(nota[j]>promedio){
 			npromedio = j;
