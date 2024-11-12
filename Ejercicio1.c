270623ea8b488b1d3735d63ad6ece55a8415a7f2
bajr06 <bryanandreujimenezrojas@gmail.com>
2024-11-12 13:39:07 +0100

    Comentarios

--- Ejercicio1.c
+++ Ejercicio1.c
@@ -13,20 +13,24 @@
 */ 
 
 int main(){
+	// Creamos las variables que estarán dentro del Array "Inventario".
 	int continente = 2;
 	int sede = 3;
 	int categoría = 4;
 	int modelo = 5;
 	int lote = 10;
-
+	
+	//El array inentario contendrá las variables asiganadas con anterioridad.
 	int inventario[continente][sede][categoría][modelo][lote];
 
+	// AL tener el array con las variables, establecemos combinaciones con resultados determinados.
 	inventario[1][3][2][1][6] = 125;
 	inventario[2][1][3][5][9] = 80;
 	inventario[2][2][1][2][3] = 90;
 	inventario[1][1][1][4][7] = 180;
 	inventario[2][3][4][3][8] = 50;
-
+	
+	//Los imprimimos por pantalla
 	printf("Hay %d unidades.\n", inventario[1][3][2][1][6]);
 	printf("Hay %d unidades.\n", inventario[2][1][3][5][9]);
 	printf("Hay %d unidades.\n", inventario[2][2][1][2][3]);
--- Práctica3.c
+++ Práctica3.c
@@ -23,17 +23,19 @@ int main(){
 	printf("¿Cuantos productos quieres gestionar?\n");
 	scanf(" %d", &gestion_producto);
 	
+	//Dependiendo de lo que el usuario ponga, se ejecutará el programa o no.
 	if(gestion_producto <= LIM){
 		for (;i <= gestion_producto; i++){
 			printf("Escriba el nombre del producto: ");
 			scanf("%s", nombre_producto[i]);
-
+			// Al pasar por cada número, se les asigna su nombre y cantidad dependiente de lo que ponga el usuario.
 			printf("Escriba la cantidad de stock del producto: ");
 			scanf("%d", &cantidad[i]);
 		}
 		
 		printf("\n");
-
+		
+		// Cuando se den todos los valores, se imprime por pantalla la numéración, el nombre y el stock.
 		do{
 			printf("Producto %d: %s - %d\n", j, nombre_producto[j], cantidad[j]);
 			j++;
@@ -45,24 +47,26 @@ int main(){
 		printf("¿Desea reabastecer algún producto (Si/No)?\n");
 		scanf("%s", reabastecer);
 		
-		while(strcmp(reabastecer, "Si") == 0){
+		while(strcmp(reabastecer, "Si") == 0){ //Al igual que la condicional inicial, está se ejecuta si la variable es igual a "Si".
 			printf("Ingrese el producto: ");
 			scanf("%s", nomproreab[1]);
 
 			printf("Número de stocks a añadir: ");
 			scanf("%d", &aumento);
-
+			
+			// Compara el valor que le hemos dado con el resto de productos que hay
 			for(k = 1; k <= gestion_producto; k++){
 				if(strcmp(nomproreab[1], nombre_producto[k]) == 0){
-					cantidad[k] += aumento;
+					cantidad[k] += aumento; //Si comprueba que es el mismo, le aumenta la cantidad que le hemos dado
 					printf("Ahora el producto %s tiene un total de %d stocks.\n", nombre_producto[k], cantidad[k]);
 				}
 			}
-
+			// Si queremos que se vuelva a ejecutar, se aplica la misma condicional.
 			printf("¿Desea reabastecer otro producto?\n");
 			scanf("%s", reabastecer);
 		}
 		
+		//Compara la cantidad de productos con 5, y dependiendo de si es menor que este, lo imprime o no.
 		printf("Productos en baja existencia\n");
 		for(; l <= gestion_producto; l++){
 			if(cantidad[l] < 5){
@@ -72,7 +76,8 @@ int main(){
 
 		printf("¿Desea buscar algún producto (Si/No)?\n");
 		scanf("%s", buscar);
-
+		
+		//
 		while(strcmp(buscar, "Si") == 0){
 			printf("Ingrese el nombre del producto: ");
 			scanf("%s", buspro[1]);
