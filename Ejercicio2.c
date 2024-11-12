a4615db841b834337e40aa52477ea483367ff495
Leonardo Marescutti <155202752+Lancelot-Mar@users.noreply.github.com>
2024-11-12 19:15:49 +0100

    Update Ejercicio2.c

--- Ejercicio2.c
+++ Ejercicio2.c
@@ -13,17 +13,16 @@
 */ 
 
 int main() {
-    char palabras[LIM][20] = {"Hola","que","tal","como","estas"};  // Array de palabras 
+    char palabras[LIM][20] = {"Hola", "que", "tal", "como", "estas"};  // Array de palabras 
 
-    //Creamos un bucle que vaya palabra por palabra guardando las palabras que no contengan el valor a,A.
     printf("Palabras sin la letra 'a':\n");
     for (int j = 0; j < LIM; j++) {
-	// saltamos las palabras que contengan lo siguiente:
-        if (strcmp(palabras[j], "a") == 0 || strcmp(palabras[j], "A") == 0) { 
-		continue;
+        // Si encontramos 'a' o 'A' en la palabra, usamos continue para saltar a la siguiente
+        if (strchr(palabras[j], 'a') || strchr(palabras[j], 'A')) {
+            continue;
         }
-	// si no tiewnen ese valor procerderemos a imprimirlas poor pantalla
-		printf("%s\n", palabras[j]);
+        // Imprimimos la palabra solo si no contiene 'a' ni 'A'
+        printf("%s\n", palabras[j]);
     }
     return 0;
 }
